e50f07b7d9ea87e55d734b731aed08ce70d7eb74
Xieisabug <136419808@qq.com>
2024-05-25 00:02:01 +0000

    change to other llm, and language to chinese add more options to run or copy command

--- shell_helper.py
+++ shell_helper.py
@@ -1,24 +1,36 @@
-#!/home/nox/micromamba/envs/shell-helper/bin/python
+#!/usr/bin/python3
 
-import sys, re, subprocess, os
+import os
+import re
+import subprocess
+import sys
 
 import openai
+from openai import OpenAI
 
-API_KEYFILE = os.path.join(os.path.expanduser("~"), ".config", "shell-helper", "key")
+OS_TYPE = "MacOS"
+TERMINAL_TYPE = "Bash"
+API_BASE = "https://api.lingyiwanwu.com/v1"
+API_KEY = "YOUR API KEY"
 
 # Set up your OpenAI API key
-with open(API_KEYFILE, 'r') as file:
-    openai.api_key = file.read().strip()
+client = OpenAI(
+    api_key=API_KEY,
+    base_url=API_BASE
+)
+
 
 def get_chatgpt_response(prompt):
-    response = openai.ChatCompletion.create(
-        model="gpt-4o"
+    completion = client.chat.completions.create(
+        model="yi-large-turbo",
         messages=[
-            {"role": "system", "content": "You write modern, excellent concise bash oneliners. All codeblocks must start with the appropriate language identifiers."},
+            {"role": "system",
+             "content": f"请根据问题回答如何使用终端命令来完成对应的工作,我的操作系统是{OS_TYPE}终端是{TERMINAL_TYPE},终端命令使用markdown code block包裹,每次仅输出一个最合适的脚本。"},
             {"role": "user", "content": prompt}
         ]
     )
-    return response.choices[0].message['content'].strip()
+    return completion.choices[0].message.content.strip()
+
 
 def extract_code_block(text):
     pattern = r"```bash\n(.*?)\n```"
@@ -27,19 +39,26 @@ def extract_code_block(text):
         return match.group(1).strip()
     return None
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: ? <prompt>")
         sys.exit(1)
 
     user_input = " ".join(sys.argv[1:])
-    prompt = f"I have the linux terminal open, I want to {user_input}. Output the correct bash one-liner to do this in a single codeblock. No japping!"
-    
-    response = get_chatgpt_response(prompt)
+    query = f"当前我打开了终端,我的问题是: {user_input}. 请告诉我如何使用脚本完成这个问题."
+
+    response = get_chatgpt_response(query)
     print(response)
 
     shell_cmd = extract_code_block(response)
     if shell_cmd:
-        user_input = input("Shell command detected. Execute?")
-        if user_input.lower() == '':
+        # TODO 多个脚本命令不应该能够执行
+        user_input = input("检测到了脚本命令. 执行(r) 或者 编辑(e) 或者 退出(q): ")
+        if user_input.lower() == 'r':
             subprocess.run(shell_cmd, shell=True)
+        elif user_input.lower() == 'e':
+            os.system(f"echo '{shell_cmd}' | pbcopy")
+            print("脚本已经复制到剪贴板.")
+        else:
+            print("退出.")
